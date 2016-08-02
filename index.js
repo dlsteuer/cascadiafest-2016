6f94c635877f7efd903b555d2dd036759a3d9b6c
Daniel Steuernol <daniel@rollbar.com>
2016-08-02 16:52:47 -0700

    building rollbar demo app for cascadiafest

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 
+var accessToken = 'fc316ac1f7404dc28af26d5baed1416c';
+
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
@@ -12,6 +14,12 @@ app.set('view engine', 'ejs');
 app.get('/', function(request, response) {
   response.render('pages/index');
 });
+
+app.get('/cause_server_side_exception', function(req, res) {
+
+});
+
+app.use(rollbar.errorHandler(accessToken));
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
